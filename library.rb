55534a05ae64763026d61b7b647d2e9d4201a440
vagrant <vagrant@student-dev-box>
2014-02-07 22:02:25 +0000

    finish lessons through 'knows who borrowed a book'

--- library.rb
+++ library.rb
@@ -1,29 +1,64 @@
 
 class Book
-  attr_reader :author
+  attr_accessor :title, :author, :id, :status
 
   def initialize(title, author)
     @author = author
+    @title = title
+    @id = nil
+    @status = "available"
   end
+
+  def check_out
+    if @status == "available"
+      @status = "checked_out"
+      true
+    else
+      false
+    end
+  end
+
+  def check_in
+    @status == "checked_out" ? @status = "available" : false
+  end
+
 end
 
 class Borrower
+  attr_reader :name
   def initialize(name)
+    @name = name
   end
 end
 
 class Library
+  attr_accessor :books
   def initialize(name)
     @books = []
+    @id_counter = 100
   end
 
-  def books
+  def register_new_book(title, author)
+    new_book = Book.new(title, author)
+    new_book.id = @id_counter
+    @books << new_book
+    @id_counter += 1
   end
 
   def add_book(title, author)
   end
 
   def check_out_book(book_id, borrower)
+    book = ""
+    @books.each do |x|
+      if x.id == book_id
+        x.check_out
+        book = x
+      else
+        puts "Book not available"
+      end
+    end
+    book
   end
 
   def check_in_book(book)
